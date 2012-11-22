d7cf3afb876be11c9eb386b39123d3e00d9d4462
onemade <onemade@ubuntu.(none)>
2012-11-22 16:21:15 +0800

    ror

---
+++ ror/viewer-server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ViewerServer::Application.config.secret_token = 'd583078beb9fd7e7bc9e376ce450ff7f9dc478a2121951b5b19e14adc4799363ec104e6cfedd86158ba908e6b6d793f3cf82c4fef81a0f32e057b3d7978e3311'
---
+++ ror/viewer-server/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ViewerServer::Application.config.session_store :cookie_store, key: '_viewer-server_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ViewerServer::Application.config.session_store :active_record_store
