847dde03aef283d1806226e2e674865c02356974
onemade <onemade@ubuntu.(none)>
2012-12-24 19:22:43 +0800

    delete eBook-server

--- ror/eBook-server/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-EBookServer::Application.config.secret_token = '06a48e4e9bd04203bd587608137c191f56599364162265ed2e61f3149c8c1b564f4937604ee5b9f789ff77a0a05e496124535bee91239d7c2bcdc96ee061bd39'
